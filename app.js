92ed73a8d9cbd3cf16b7ecea87e2437cea066276
root <root@srv461821.hstgr.cloud>
2024-01-17 12:38:28 +0000

    feat: add .env

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`Solicitud recibida: ${req.method} ${req.url}`);
+  next();
+});
+
 app.use("/api/users", userRoutes);
 app.use('/api/roles', roleRoutes);
 app.use('/api/platforms', platformRoutes);
