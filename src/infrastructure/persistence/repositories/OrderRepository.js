1bdeee4f391af7912d1cddec3a062fb6c166eec6
Daniel Betancur <daniel.b@somosgentedigital.com>
2024-07-17 12:55:01 -0500

    feat: add populate city to get orders

--- src/infrastructure/persistence/repositories/OrderRepository.js
+++ src/infrastructure/persistence/repositories/OrderRepository.js
@@ -13,7 +13,8 @@ class OrderRepository {
 
   async getAll(filters) {
     try {
-      const orders = await OrderModel.find(filters).sort({ date: -1 }).limit(50).populate('shop').populate('orderDetails.product');
+      const orders = await OrderModel.find(filters).sort({ date: -1 }).limit(50)
+          .populate('cityId').populate('shop').populate('orderDetails.product');
       return orders;
     } catch (error) {
       throw new Error(`Error while fetching orders: ${error.message}`);
