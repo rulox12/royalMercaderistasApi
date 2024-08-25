09d41d4d8915eeabd221cdc9be27b88f39a061c2
Daniel Betancur <daniel.b@somosgentedigital.com>
2024-08-24 22:39:47 -0500

    feat: add script update orders for add platform Id

--- src/infrastructure/persistence/models/OrderModel.js
+++ src/infrastructure/persistence/models/OrderModel.js
@@ -1,49 +1,49 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const {Schema} = mongoose;
 
 const OrderSchema = new Schema({
-  date: {
-    type: Date,
-    required: true,
-  },
-  shop: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Shop",
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  cityId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "City",
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  platform: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Platform",
-    required: true,
-  },
-  orderDetails: [{
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true,
+    date: {
+        type: Date,
+        required: true,
     },
-    INVE: String,
-    AVER: String,
-    LOTE: String,
-    RECI: String,
-    PEDI: String,
-    VENT: String,
-    PEDI_REAL: String,
-  }],
+    shop: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Shop",
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    cityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "City",
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    platform: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Platform",
+        required: true,
+    },
+    orderDetails: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        INVE: String,
+        AVER: String,
+        LOTE: String,
+        RECI: String,
+        PEDI: String,
+        VENT: String,
+        PEDI_REAL: String,
+    }],
 });
 
 const OrderModel = mongoose.model('Order', OrderSchema);
